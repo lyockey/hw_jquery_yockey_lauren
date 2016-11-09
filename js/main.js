d938c0866f0854f77f9a7a1204858277de0b2312
Lauren Yockey <laurenyockey@gmail.com>
2016-11-09 13:16:31 -0600

    change styles of all paragraph elements

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 $(document).ready(function () {
-
+    var paragraphs = $('p');
+    $(paragraphs).css({'color': '#164084', 'font-weight': 'bold', 'font-size': '0.9em', 'line-height': '2em', 'margin': '2em'});
 });
